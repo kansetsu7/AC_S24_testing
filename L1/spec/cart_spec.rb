5c19093d5f62ec903dfaea768d2fdc6eb005e1f8
Kansetsu Kuo <kansetsu4@gmail.com>
2018-05-10 14:22:50 +0800

    Done: reconstruct spec for no discount

--- L1/spec/cart_spec.rb
+++ L1/spec/cart_spec.rb
@@ -1,17 +1,20 @@
 require_relative '../lib/cart.rb'
 
 describe Cart do
-
-  it '買第一集1本' do
+  before do
     @cart = Cart.new
-    @cart.add({ "1st": 1, "2nd": 0, "3rd": 0, "4th": 0, "5th": 0 })
-    expect(@cart.calculate).to eq(100)
   end
 
-  it '買第一集3本' do
-    @cart = Cart.new
-    @cart.add({ "1st": 3, "2nd": 0, "3rd": 0, "4th": 0, "5th": 0 })
-    expect(@cart.calculate).to eq(300)
+  context '1. no discount' do
+    it '買第一集1本' do
+      @cart.add({ "1st": 1, "2nd": 0, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(100)
+    end
+
+    it '買第一集3本' do
+      @cart.add({ "1st": 3, "2nd": 0, "3rd": 0, "4th": 0, "5th": 0 })
+      expect(@cart.calculate).to eq(300)
+    end
   end
 
 end
